0eb18d1f573aedda444fc68191e343f8ffe476d1
pikruse <pikruser@gmail.com>
2024-10-31 15:09:18 -0400

    finished graph llm, started working on graph encoders

--- utils/GraphEncoder.py
+++ utils/GraphEncoder.py
@@ -1,1 +1,102 @@
 # all of our graph embedding methods
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, TransformerConv, GATConv
+
+class GCN(nn.Module):
+
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout, num_heads=-1):
+        super(GCN, self).__init__()
+
+        # inital conv layer
+        self.convs = nn.ModuleList()
+        self.convs.append(GCNConv(in_channels, hidden_channels))
+
+        # initial bn layer
+        self.bns = nn.ModuleList()
+        self.bns.append(nn.BatchNorm1d(hidden_channels))
+
+        # hidden conv + bn stack
+        for _ in range(num_layers = 2):
+            self.convs.append(GCNConv(hidden_channels, hidden_channels))
+            self.bns.append(nn.BatchNorm1d(hidden_channels))
+        
+        # final conv 
+        self.convs.append(GCNConv(hidden_channels, out_channels))
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+    
+    # forward pass - since these are the only trainable modules in the paper
+    def forward(self, x, adj_t, edge_attr):
+
+        # loop through all convs except last
+        for i, conv in enumerate(self.convs[:-1]):
+            x = conv(x, adj_t)
+            x = self.bns[i](x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        
+        # final layer
+        x = self.convs[-1](x, adj_t)
+
+        # return input graph + edge attributes
+        return x, edge_attr
+
+class GraphTransformer(nn.Module):
+    
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout, num_heads=-1):
+        super(GraphTransformer, self).__init__()
+
+        # initial transformer + bn
+        self.convs = nn.ModuleList()
+        self.convs.append(TransformerConv(in_channels=in_channels, 
+                                          out_channels=hidden_channels//num_heads, 
+                                          heads=num_heads, 
+                                          edge_dim=in_channels, 
+                                          dropout=dropout))
+        
+        self.bns = nn.ModuleList()
+        self.bns.append(nn.BatchNorm1d(hidden_channels))
+
+        # hidden transformer + bn
+        for _ in range(num_layers - 2):
+            self.convs.append(TransformerConv(in_channels=hidden_channels,
+                                              out_channels=hidden_channels//num_heads,
+                                              heads=num_heads,
+                                              edge_dim=in_channels,
+                                              dropout=dropout))
+            self.bns.append(nn.BatchNorm1d(hidden_channels))
+        
+        # final transformer + drop
+        self.convs.append(TransformerConv(in_channels=hidden_channels,
+                                          out_channels=out_channels//num_heads,
+                                          edge_dim=in_channels,
+                                          dropout=dropout))
+        self.dropout = dropout
+        
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+    
+    def forward(self, x, adj_t):
+        pass
+
+class GAT():
+    
+    def __init__(self):
+        pass
+
+load_gnn_model = {
+    'gcn': GCN,
+    'gat': GAT,
+    'gt': GraphTransformer
+}
+    
---
+++ utils/LLM.py
@@ -0,0 +1,268 @@
+# imports
+import contextlib
+import torch
+import torch.nn as nn
+from torch.cuda.amp import autocast as autocast
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from torch_scatter import scatter
+from GraphEncoder import load_gnn_model
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    prepare_model_for_kbit_training
+)
+
+# define special tokens
+BOS = '<s>[INST]'
+EOS_USER = '[/INST]'
+EOS = '<s>'
+
+# define mask token
+IGNORE_INDEX = -100
+
+class GraphLLM(nn.Module):
+    """
+    Graph LLM object, re-implemented from G-Retriever: https://github.com/XiaoxinHe/G-Retriever/blob/main/src/model/graph_llm.py
+    """
+
+    def __init__(self, args, **kwargs):
+        super().__init__()
+        self.max_tx_len = args.max_txt_len
+        self.max_new_tokens = args.max_new_tokens
+
+        print('Loading LLaMA...')
+        kwargs = {
+            "max_memory": {0: '80GiB', 1: '80GiB'}, # do we need this?
+            "device_map": "auto",
+            "revision": "main"
+        }
+
+        # create tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(args.llm_model_path, 
+                                                       use_fast=False, 
+                                                       revision=kwargs["revision"])
+        self.tokenizer.pad_token_id = 0
+        self.tokenizer.padding_side = 'left'
+
+        # create model
+        model = AutoModelForCausalLM.from_pretrained(args.llm_model_path,
+                                                     torch_dtype=torch.float16,
+                                                     low_cpu_mem_usage=True,
+                                                     **kwargs)
+        
+        # freeze model if specified
+        if args.llm_frozen == "True":
+            print("Freezing LLaMA!")
+            for name, param in model.named_parameters():
+                param.requires_grad = False # don't update weights for these layers
+        else:
+            print("Training LLaMA with LORA!")
+            model = prepare_model_for_kbit_training(model)
+
+            # lora params
+            lora_r: int = 8
+            lora_alpha: int = 16
+            lora_dropout: float = 0.05
+            lora_target_modules = ["q_proj", "v_proj"]
+
+            config = LoraConfig(
+                r=lora_r,
+                lora_alpha=lora_alpha,
+                target_modules = lora_target_modules,
+                lora_dropout = lora_dropout,
+                bias="none",
+                task_type="CAUSAL_LM"
+            )
+            
+        # load autoregressive component
+        model = get_peft_model(model, config)
+        self.model = model
+
+        # load graph encoder component - load_gnn_model function is from GraphEncoder.py 
+        self.graph_encoder = load_gnn_model[args.gnn_model_name](
+            in_channels = args.gnn_in_dim,
+            out_channels = args.gnn_hidden_dim,
+            hidden_channels = args.gnn_hidden_dim,
+            num_layers = args.gnn_num_layers,
+            dropout = args.gnn_dropout,
+            num_heads=args.gnn_num_heads
+        ).to(self.model.device)
+
+        # add MLP to project graph encoder output to match llm input
+        self.projector = nn.Sequential(
+            nn.Linear(args.gnn_hidden_dim, 2048),
+            nn.Sigmoid(),
+            nn.Linear(2048, 4096)
+        ).to(self.model.device)
+
+        # get word embeddings - where is this coming from??
+        self.word_embedding = self.model.model.get_input_embeddings
+
+    # property decorator allows us to call the method without any parameters, and you can assign values to it
+    @property
+    def device(self):
+        return list(self.parameters())[0].device
+
+    def maybe_autocast(self, dtype=torch.bfloat16):
+        # if on cpu, don't autocast
+        # if on gpu, use autocast with dtype if provided, otherwise use torch.bfloat16
+        enable_autocast = self.device != torch.device("cpu")
+
+        if enable_autocast:
+            return torch.amp.autocast('cuda', dtype=dtype)
+        else:
+            return contextlib.nullcontext() # return a context manager if autocast off
+    
+    def encode_graphs(self, samples):
+        # use graph encoder module to "compress" graph dimensions
+        graphs = samples['graph']
+        graphs = graphs.to(self.model.device)
+        n_embeds, _ = self.graph_encoder(graphs.x, graphs.edge_index.long(), graphs.edge_attr) # might need to change this depending on graph info we have
+
+        # mean pooling: reduce objects along given dimension with reduction operation
+        g_embeds = scatter(n_embeds, graphs.batch, dim=0, reduce='mean')
+
+        return g_embeds
+    
+    # forward pass (training)
+    def forward(self, samples):
+
+        # encode description, questions, and labels
+        questions = self.tokenizer(samples["question"], add_special_tokens=False)
+        descriptions = self.tokenizer(samples["desc"], add_special_tokens=False)
+        labels = self.tokenizer(samples["labels"], add_special_tokens=False)
+
+        # encode special tokens
+        eos_tokens = self.tokenizer(EOS, add_special_tokens=False)
+        eos_user_tokens = self.tokenizer(EOS_USER, add_special_tokens=False)
+        bos_embeds = self.word_embedding(self.tokenizer(BOS, add_special_tokens=False, return_tensors='pt').input_ids[0])
+        pad_embeds = self.word_embedding(torch.tensor(self.tokenizer.pad_token_id)).unsqueeze(0)
+
+        # encode graphs
+        graph_embeds = self.encode_graphs(samples) # encode_graphs only looks at graph description stuff
+        graph_embeds = self.projector(graph_embeds)
+
+        # batch processing
+        batch_size = len(samples['id'])
+        batch_inputs_embeds = []
+        batch_attention_mask = []
+        batch_label_input_ids = []
+
+        # go batch-by-batch
+        for i in range(batch_size):
+
+            # add bos, eos token
+            label_input_ids = labels.input_ids[i][:self.max_new_tokens] + eos_tokens.input_ids
+
+            # combine the label input ids with the desc, questions, and special tokens to make full string
+            input_ids = descriptions.input_ids[i][:self.max_txt_len] + questions.input_ids[i] + eos_user_tokens.input_ids + label_input_ids 
+            
+            # embed input
+            inputs_embeds = self.word_embedding(torch.tensor(input_ids).to(self.model.device))
+            inputs_embeds = torch.cat([bos_embeds, graph_embeds[i].unsqueeze(0), inputs_embeds], dim=0)
+
+            batch_inputs_embeds.append(inputs_embeds)
+            batch_attention_mask.append([1] * inputs_embeds.shape[0]) # attend to all tokens
+
+            # mask out the input graph, description, question in our labels
+            label_input_ids = [IGNORE_INDEX] * (inputs_embeds.shape[0] - len(label_input_ids)) + label_input_ids 
+
+        # pad input_embeds
+        max_length = max([x.shape[0] for x in batch_inputs_embeds])
+        for i in range(batch_size):
+            pad_length = max_length - batch_inputs_embeds[i].shape[0]
+            batch_inputs_embeds[i] = torch.cat([pad_embeds.repeat(pad_length, 1), batch_inputs_embeds[i]])
+            batch_attention_mask[i] = [0] * pad_length + batch_label_input_ids[i]
+            batch_label_input_ids[i] = [IGNORE_INDEX] * pad_length + batch_label_input_ids[i] # mask everything that's not part of the answer - we only want the model to output this 
+        
+        # convert lists into tensors
+        inputs_embeds = torch.stack(batch_inputs_embeds, dim = 0).to(self.model.device)
+        attention_mask = torch.tensor(batch_attention_mask).to(self.model.device)
+        label_input_ids = torch.tensor(batch_label_input_ids).to(self.model.device)
+
+        # send inp, mask, and labels to the model
+        with self.maybe_autocast():
+            outputs = self.model(
+                inputs_embeds=inputs_embeds,
+                attention_mask=attention_mask,
+                labels=label_input_ids
+            )
+
+        # they only return the loss, hmmm....
+        return outputs.loss
+    
+    # forward pass (inference)
+    def inference(self, samples):
+
+        # encode descriptions and questions
+        questions = self.tokenizer(samples["question"], add_special_tokens=False)
+        descriptions = self.tokenizer(samples["desc"], add_special_tokens=False)
+
+        # encode special tokens
+        eos_user_tokens = self.tokenizer(EOS_USER, add_special_tokens=False)
+        bos_embeds = self.word_embedding(self.tokenizer(BOS, add_special_tokens=False, return_tensors='pt').input_ids[0])
+        pad_embeds = self.word_embedding(torch.tensor(self.tokenizer.pad_token_id)).unsqueeze(0)
+
+        # encode graphs
+        graph_embeds = self.encode_graphs(samples)
+        self.projector(graph_embeds)
+
+        # batch processing
+        batch_size = len(samples['id'])
+        batch_inputs_embeds = []
+        batch_attention_mask = []
+        for i in range(batch_size):
+            # add bos, eos
+            input_ids = descriptions.input_ids[i][:self.max_txt_len] + questions.input_ids[i] + eos_user_tokens.input_ids
+            inputs_embeds = self.word_embedding(torch.tensor(input_ids).to(self.model.device))
+            inputs_embeds = torch.cat([bos_embeds, graph_embeds[i].unsqueeze(0), inputs_embeds], dim=0)
+
+            batch_inputs_embeds.append(inputs_embeds)
+            batch_attention_mask.appen([1] * inputs_embeds.shape[0])
+        
+        # pad inputs_embeds
+        max_length = max([x.shape[0] for x in batch_inputs_embeds])
+        for i in range(batch_size):
+            pad_length = max_length - batch_inputs_embeds[i].shape[0]
+            batch_inputs_embeds[i] = torch.cat([pad_embeds.repeat(pad_length, 1), batch_inputs_embeds[i]])
+            batch_attention_mask[i] = [0] * pad_length + batch_attention_mask[i]
+
+        input_embeds = torch.stack(batch_inputs_embeds, dim=0).to(self.model.device)
+        attention_mask = torch.stack(batch_attention_mask).to(self.model.device)
+
+        with self.maybe_autocast():
+            outputs = self.model.generate(
+                inputs_embeds=inputs_embeds,
+                max_new_tokens=self.max_new_tokens,
+                attention_mask=attention_mask,
+                use_cache=True # IMPORTANT (apparently, not sure why)
+            )
+        
+        # convert tokens back to text
+        pred = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+        # return dict of pred + other info
+        return {'id': samples['id'],
+                'pred': pred,
+                'label': samples['label'],
+                'question': samples['question'],
+                'desc': samples['desc']}
+    
+    def print_trainable_params(self):
+        trainable_params = 0
+        all_param = 0
+
+        for _, param in self.named_parameters():
+            num_params = param.numel()
+            all_param += num_params
+            if param.requires_grad:
+                trainable_params += num_params
+        
+        return f"Trainable parameters: {trainable_params} | Total parameters: {all_param} | {(trainable_params / all_param):.2%} trainable "
+
+
+
+
+
+
+        
