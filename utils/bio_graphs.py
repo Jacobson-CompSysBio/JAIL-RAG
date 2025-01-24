68d7d6c12bd618661a6ed661df277e1ffe1042e6
kpsmithjr <kpsmithjr@gmail.com>
2025-01-24 15:30:40 -0500

    Load graph from q/a file

--- utils/bio_graphs.py
+++ utils/bio_graphs.py
@@ -20,7 +20,7 @@ class BiologicalDataset(Dataset):
   
   def __getitem__(self, index: int):
     text = self.text.iloc[index]
-    graph = torch.load(f'{self.path}/graphs/{index}.pt')
+    graph = torch.load(text['graph'])
     graph.x = torch.tensor(graph.x.T, dtype=torch.float32)
     graph.edge_index = graph.edge_index.type(torch.int64)
 
