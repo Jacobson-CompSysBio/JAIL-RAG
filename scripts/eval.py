c49c6584b4d4ec3cc9d7585c1ac2ad097280d6b9
pikruse <pikruser@gmail.com>
2025-02-15 11:19:27 -0500

    scripted version of eval

--- scripts/eval.py
+++ scripts/eval.py
@@ -1,20 +1,20 @@
 import os, sys
-import json
 import wandb
-import tqdm.notebook as tqdm
+from tqdm.notebook import tqdm
 import transformers
 import torch
 import torch.nn as nn
 from torch.nn.utils import clip_grad_norm_
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, Subset
 from importlib import reload
 from torch_scatter import scatter
 from transformers import pipeline
+from IPython.display import clear_output
 
 sys.path.append('../')
 
 from utils import preprocess as pp
-from utils.evaluate import eval_funcs
+from utils.evaluate import eval_funcs, normalize
 from utils.collate import collate_fn 
 from utils.ckpt import _reload_best_model
 from utils.graph_llm import GraphLLM
@@ -24,24 +24,26 @@ from utils.textualize import *
 from utils.bio_graphs import BiologicalDataset
 from utils.seed import seed_everything
 
-# ----------------------------------------------------------
+# -------
 # OPTIONS
+# -------
+verbose = False
 seed = 42
 seed_everything(seed)
 
 batch_size = 1
-data_path = '../data/DREAM4_gold_standards/connections_node_label'
-model_path = '../checkpoints/test_run/epoch_5_best.pth' # REPLACE WITH BEST MODEL PATH
-eval_path = '../logs/train/eval.json'
+data_path = '../data/subgraphs/all'
+model_path = '../checkpoints/graph_llm_fsdp/epoch_1_best.pth' # REPLACE WITH BEST MODEL PATH
+eval_path = '../logs/eval/graph_llm_fsdp/'
 
-# ----------------------------------------------------------
+# --------------------
 # DATASET / DATALOADER
-
+# --------------------
 dataset = BiologicalDataset(data_path)
 idx_split = dataset.get_idx_split()
 
 # split datasets on idx
-test_dataset = [dataset[i] for i in idx_split["test"]]
+test_dataset = Subset(dataset, idx_split['test'])
 
 test_loader = DataLoader(test_dataset, 
                           batch_size=batch_size,
@@ -50,8 +52,9 @@ test_loader = DataLoader(test_dataset,
                           shuffle=True,
                           collate_fn=collate_fn)
 
-# ----------------------------------------------------------
+# ----------
 # LOAD MODEL
+# ----------
 base = GraphLLM(max_text_len=512,
                      max_max_new_tokens=32,
                      max_memory=[80, 80],
@@ -61,20 +64,58 @@ base = GraphLLM(max_text_len=512,
 
 model = _reload_best_model(base, model_path)
 
-# ----------------------------------------------------------
+# --------
 # EVALUATE
-model.eval()
-progress_bar_test = tqdm(range(len(test_loader)))
-with open(eval_path, "w") as f:
-    for _, batch in enumerate(test_loader):
-        with torch.no_grad():
-            output = model.inference(batch)
-            df = pd.DataFrame(output)
-            for _, row in df.iterrows():
-                f.write(json.dumps(dict(row)) + "\n")
-        progress_bar_test.update(1)
+# --------
+# options
+loader = test_loader
 
-# Step 5. Post-processing & Evaluating
-acc = eval_funcs[dataset](eval_path)
-print(f'Test Acc {acc}')
-# wandb.log({'Test Acc': acc})
+# set to eval
+model.model.generation_config.pad_token_id = model.tokenizer.pad_token_id
+model.eval()
+
+n_correct = 0
+i = 0
+# loop through dataloader
+for batch in tqdm(loader):
+    out = model.inference(batch)
+    pred = out['pred']
+    actual = out['label']
+    # test accuracy
+    for p, a in zip(pred, actual):
+        p_ans, p_think = normalize(p)
+        a = str(a)
+        if verbose:
+            print(p_think)
+            print(p_ans)
+            print(a)
+            print()
+        if a in p_ans:
+            n_correct += 1
+            if verbose:
+                print("Correct!")
+                print()
+        else:
+            if verbose:
+                print("Incorrect :(")
+                print()
+        i += 1
+    print(f"Accuracy: {n_correct/i:.2%} | {n_correct}/{i}", end='\r')
+acc = n_correct / i
+print(f"Accuracy: {acc:.2%} | {n_correct}/{i}")
+
+# ------------------
+# PRINT EXAMPLE EVAL
+# ------------------
+with open(f'{eval_path}/eval.csv', 'w') as f:
+    f.write("actual,prediction,reasoning")
+    rand_idx = torch.randint(0, len(test_dataset), (1,)).item()
+    example_batch = test_dataset[rand_idx]
+    
+    out = model.inference(batch)
+    pred = out['pred']
+    actual = out['label']
+    for p, a in zip(pred, actual):
+        p_ans, p_think = normalize(p)
+        a = str(a)
+        f.write(f"{a},{p_ans},{p_think}")
--- utils/bio_graphs.py
+++ utils/bio_graphs.py
@@ -29,7 +29,7 @@ class BiologicalDataset(Dataset):
       'question': text['question'],
       'scope': text['scope'],
       'label': text['label'],
-      'desc': 'You will be given a biological graph and a question. Provide an answer of YES of NO based on the question and the given input graph. Surround your answer in <answer> and </answer> tags. Surround any reasoning with <think> and </think> tags.',
+      'desc': 'You will be given a biological graph and a question. Provide an answer of YES of NO based on the question and the given input graph. Explain your reasoning, and surround it with with <think> and </think> tags. Then provide an answer, surrounded in <answer> and </answer> tags. ',
       'graph': graph,
     }
   
